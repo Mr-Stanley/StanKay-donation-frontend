36573ad96967cd08809cdd3a1a69651e170e6b0d
Stanleys world <Stanleyugoo5@gmail.com>
2025-05-08 13:58:50 +0100

    another commit

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SignupPage from '../pages/SignupPage.jsx';
+// import SignupPage from '../pages/SignupPage.jsx';
 import Layout from '../components/Layout.jsx';
 import LoginPage from '../pages/LoginPage.jsx';
 import DonateNowPage from '../pages/DonateNowPage.jsx';
